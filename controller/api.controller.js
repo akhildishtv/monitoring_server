37909b7664de56faad2184f1d9301a17a96900fb
Akhil Kumar Gupta <awadhesh.soni@dishtv.in>
2021-12-10 16:23:46 +0530

    new change

--- controller/api.controller.js
+++ controller/api.controller.js
@@ -3,16 +3,16 @@ var APISCHEMA = require('../models/api.schema')
 const saveAPIData = async (req, res) => {
     let postData = req.body;
     try {
-      let data = await saveData(postData)
-      if(data){
-        return res.json({ data: data, status: "Success", code: 200, msg: "Data Saved Successfully.!" })
-      }
+        let data = await saveData(postData)
+        if (data) {
+            return res.json({ data: data, status: "Success", code: 200, msg: "Data Saved Successfully.!" })
+        }
     } catch (err) {
-      if (err) {
-        return res.json({ status: "Failure", code: 301, msg: "Something went wrong.!" });
-      }
+        if (err) {
+            return res.json({ status: "Failure", code: 301, msg: "Something went wrong.!", err: err });
+        }
     }
-  }
+}
 
 let saveData = body => new Promise((resolve, reject) => {
     APISCHEMA.create(body, (err, data) => {
@@ -23,4 +23,7 @@ let saveData = body => new Promise((resolve, reject) => {
         }
     });
 });
-  exports.saveAPIData = saveAPIData;
+
+
+
+exports.saveAPIData = saveAPIData;
--- models/api.schema.js
+++ models/api.schema.js
@@ -1,13 +1,25 @@
 var mongoose = require('mongoose');
-var mongoosePaginate = require('mongoose-paginate');
 var Schema = mongoose.Schema;
 
-var Schema = new Schema({
-    title: { type: String, required: "title name is required." },
-    abbreviation: { type: String, default: "" },
-    isActive: { type: Boolean, default: true },
-    isDeleted: { type: Boolean, default: false }
-
-}, { timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } });
-Schema.plugin(mongoosePaginate);
+var Schema = new Schema(
+    {
+        title: {
+            type: String, required: "title name is required."
+        },
+        abbreviation: {
+            type: String, default: ""
+        },
+        isActive: {
+            type: Boolean, default: true
+        },
+        isDeleted: {
+            type: Boolean, default: false
+        }
+    }, {
+    timestamps:
+    {
+        createdAt: 'createdAt',
+        updatedAt: 'updatedAt'
+    }
+});
 module.exports = mongoose.model('realTime-API', Schema);
