4ceec8dd15af7f9c096c74a730310042a6e0bb6a
Akhil Kumar Gupta <awadhesh.soni@dishtv.in>
2021-12-10 18:16:58 +0530

    new changes

--- controller/api.controller.js
+++ controller/api.controller.js
@@ -14,6 +14,23 @@ const saveAPIData = async (req, res) => {
     }
 }
 
+const getAPIData = async (req, res) => {
+    let postData = req.query;
+    try {
+        let query = {
+
+        }
+        let data = await APISCHEMA.find(query)
+        if (data) {
+            return res.json({ data: data, status: "Success", code: 200, msg: "Data Saved Successfully.!" })
+        }
+    } catch (err) {
+        if (err) {
+            return res.json({ status: "Failure", code: 301, msg: "Something went wrong.!", err: err });
+        }
+    }
+}
+
 let saveData = body => new Promise((resolve, reject) => {
     APISCHEMA.create(body, (err, data) => {
         if (err) {
@@ -27,3 +44,5 @@ let saveData = body => new Promise((resolve, reject) => {
 
 
 exports.saveAPIData = saveAPIData;
+exports.getAPIData = getAPIData;
+
--- routes/api.js
+++ routes/api.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var APICONTROLLER = require('../controller/api.controller')
 /* GET users listing. */
 router.post('/saveAPIData', APICONTROLLER.saveAPIData);
+router.get('/getAPIData', APICONTROLLER.getAPIData);
 
 module.exports = router;
